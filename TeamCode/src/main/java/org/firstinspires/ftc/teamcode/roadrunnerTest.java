a7096c8e1e4b2543328181cf09f819d0f106b325
klee111287 <54005889+klee111287@users.noreply.github.com>
2022-02-04 17:16:59 +0100

    Init

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunnerTest.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+public class roadrunnerTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Trajectory myTrajectory = drive.trajectoryBuilder(new Pose2d())
+                .strafeRight(10)
+                .splineTo(new Vector2d(40, 40), Math.toRadians(90))
+                .forward(5)
+                .build();
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        drive.followTrajectory(myTrajectory);
+    }
+}
+
